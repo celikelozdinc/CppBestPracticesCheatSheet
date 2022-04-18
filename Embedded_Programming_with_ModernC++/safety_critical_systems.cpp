1164085d1518d338dedd701397609177846d85e2
celikelozdinc <niyazi.celikel@ulakhaberlesme.com.tr>
2022-04-18 19:22:59 +0300

    feat : user-defined literals : introduction
    Signed-off-by: celikelozdinc <niyazi.celikel@ulakhaberlesme.com.tr>

--- Embedded_Programming_with_ModernC++/safety_critical_systems.cpp
+++ Embedded_Programming_with_ModernC++/safety_critical_systems.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ostream>
 #include <vector>
 #include <array>
 #include <set>
@@ -12,6 +13,40 @@ struct InitializationData{
     }
 };
 
+namespace Distance {
+    class MyDistance {
+    public:
+        MyDistance(double i) : m{i} {}
+        friend std::ostream& operator<< (std::ostream& out, const MyDistance& dist) {
+            out << dist.m << " m";
+            return out;
+        } 
+    private:
+        double m; //meter
+    };
+    namespace Unit {
+        MyDistance operator"" _km(long double d) {
+            return MyDistance(1000*d);
+        }
+        MyDistance operator"" _m(long double m) {
+            return MyDistance(m);
+        }
+
+        MyDistance operator"" _cm(long double c) {
+            return MyDistance(c/100);
+        }
+
+        MyDistance operator"" _ft(long double f) {
+            return MyDistance(f*0.3048);
+        }
+
+        MyDistance operator"" _mi(long double mi) {
+            return MyDistance(mi*1609.344);
+        }
+    }
+}
+
+using namespace Distance::Unit;
 
 int main() {
     /*************************** Uniform initialization with {}**********************/
@@ -40,6 +75,15 @@ int main() {
 
     std::pair<int, std::string> pa = std::make_pair(1, std::string("second"));
     //std::tuple<std::string, int, double, bool, char> tup = std::make_tuple(std:string("xxx"), 4, 1.1, true, 'a');
+    /*********************************************************************************/
+    /***************** User-Defined Literals : Introduction **************************/
+    std::cout << "2.0_mi => " << 2.0_mi << "\n";
+    std::cout << "1.0_mi => " << 1.0_mi << "\n";
+    std::cout << "1.0_km => " << 1.0_km << "\n";
+    std::cout << "1.0_m => " << 1.0_m << "\n";
+    std::cout << "2.0_ft => " << 2.0_ft << "\n";
+    std::cout << "1.0_ft => " << 1.0_ft << "\n";
+    std::cout << "1.0_cm => " << 1.0_cm << "\n";
     /*********************************************************************************/
 
     return 0;
