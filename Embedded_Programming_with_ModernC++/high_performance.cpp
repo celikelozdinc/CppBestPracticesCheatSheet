c4740e742dbc43d5b66a446c47db93c2664daf33
celikelozdinc <niyazi.celikel@ulakhaberlesme.com.tr>
2022-04-28 09:02:51 +0300

    feat : associative containers
    Signed-off-by: celikelozdinc <niyazi.celikel@ulakhaberlesme.com.tr>

--- Embedded_Programming_with_ModernC++/high_performance.cpp
+++ Embedded_Programming_with_ModernC++/high_performance.cpp
@@ -1,8 +1,17 @@
 #include <type_traits>
 #include <typeinfo>
 #include <iostream>
+#include <unordered_set>
 
 constexpr int sq(int x) {return x*x;}
+
+template <typename Container>
+void print_elements(Container C) {
+    for (const auto& item : C) {
+        std::cout << '{' << item << '}' << "\n";
+    }
+    std::cout << "Bucket Count : " << C.bucket_count() << "\n";
+}
 
 int main() {
     /*************************** Type Traits **********************/
@@ -12,7 +21,6 @@ int main() {
 
     // Is const?
     std::cout << "IS CONST? : " << std::boolalpha << std::is_const<Integer>::value <<"\n";
-
 
     // Add Const
     std::cout << "const int == CI ? " << std::boolalpha << std::is_same<const int, CI>::value << "\n";
@@ -34,5 +42,45 @@ int main() {
     int arraySq[sq(2)];
     int arraySquare[sq(b)];
     /**************************************************************/
+    /******************  Associative Containers********************/
+    using Set = std::unordered_set<std::string>;
+    using MultiSet = std::unordered_multiset<std::string>;
+    Set set{"bin", "root"};
+    std::cout << "Set before insertions: \n";
+    print_elements(set);
 
+    std::cout << "Set after some inserts: \n";
+    set.insert({"etc", "tmp"});
+    print_elements(set);
+
+    auto varIter = set.find("var");
+    if (set.end() != varIter) {
+        std::cout << "Found {var} in set: " << *varIter << "\n";
+    } else {
+        std::cout << "Could not find {var} in set\n";
+    }
+    auto binIter = set.find("bin");
+    if (set.end() != binIter) {
+        std::cout << "Found {bin} in set: " << *binIter << "\n";
+    } else {
+        std::cout << "Could not find {bin} in set\n";
+    }
+
+    MultiSet multiSet{"grimm", "jaud-grimm"};
+    std::cout << "MultiSet before insertions: \n";
+    print_elements(multiSet);
+
+    multiSet.insert({"grimm", "djikstra"});  //=> Accepts initializer list
+    std::cout << "MultiSet after some inserts: \n";
+    print_elements(multiSet);
+
+    std::cout << "Multiset.count(grim) : " << multiSet.count("grim") << "\n";
+    std::cout << "Multiset.count(djikstra) : " << multiSet.count("djikstra") << "\n";
+    std::cout << "Multiset.count(grimm) : " << multiSet.count("grimm") << "\n";
+    auto iter = multiSet.equal_range("grimm");
+    std::cout << "Searching for grimm in multiset: \n";
+    for (auto it = iter.first ; it != iter.second; ++it) {
+        std::cout << *it << "\n";
+    }
+    /**************************************************************/
 }
