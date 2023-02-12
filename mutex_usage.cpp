c268ad757426ee3b885f21d2ffcfc937a12b361c
celikelozdinc <niyazi.celikel@ulakhaberlesme.com.tr>
2023-02-12 20:21:23 +0300

    feat : mutex usage
    Signed-off-by: celikelozdinc <niyazi.celikel@ulakhaberlesme.com.tr>

---
+++ mutex_usage.cpp
@@ -0,0 +1,44 @@
+/**
+ * g++ mutex_usage.cpp -lpthread -o mutex ; ./mutex ; rm -rf ./mutex
+ * https://medium.com/@simontoth/daily-bit-e-of-c-std-mutex-558fe54c9dc3
+ */
+
+#include <mutex>
+#include <thread>
+#include <iostream>
+
+struct SharedData{
+  int value{0};
+  std::mutex mtx;
+};
+
+int main() {
+  SharedData initialSharedData{.value=0, .mtx{}};
+
+  auto thread1 = std::thread(
+      [&initialSharedData]()
+      {
+        for (uint8_t u = 0; u < 100; u++) {
+          std::unique_lock<std::mutex> lock(initialSharedData.mtx);
+          initialSharedData.value += 1;
+        }
+      }
+ );
+
+  auto thread2 = std::thread(
+      [&initialSharedData]()
+      {
+        for (uint8_t u = 0; u < 5; u++) {
+          std::unique_lock<std::mutex> lock(initialSharedData.mtx);
+          initialSharedData.value += 10;
+        }
+      }
+  );
+
+  thread1.join();
+  thread2.join();
+
+  std::cout << "SharedData : " << initialSharedData.value << "\n";
+
+  return EXIT_SUCCESS;
+}
