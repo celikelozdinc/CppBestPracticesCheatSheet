be01d0afeb7af7983c37425ff65bebd7cc892899
celikelozdinc <niyazi.celikel@ulakhaberlesme.com.tr>
2023-04-02 20:25:46 +0300

    reference

--- linkage/external/main.cpp
+++ linkage/external/main.cpp
@@ -6,7 +6,7 @@ extern void baz(int i);
 
 /**
  *  g++ linkage/external/main.cpp linkage/external/bar.cpp linkage/external/fubar.cpp linkage/external/common.cpp -o linkage/external/main
- * @return
+ *  Have a look at this example : http://csundergrad.science.uoit.ca/courses/cpp-notes/notes/dealing-with-multiple-files.html
  */
 int main() {
   std::cout << "++++++++++\n";
