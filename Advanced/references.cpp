8f519ba58782c49a86d38962291f14d7b0ec92a6
celikelozdinc <niyazi.celikel@ulakhaberlesme.com.tr>
2023-06-26 14:01:48 +0300

    string literals

--- Advanced/references.cpp
+++ Advanced/references.cpp
@@ -51,18 +51,17 @@ int main () {
   int x{}, y{};
   Widget w1{}, w2{};
   std::cout << "***************1******************\n";
-  printvalcat<decltype((x))>();
-  printvalcat<decltype((y))>();
+  printvalcat<decltype((x))>(); //=> LVALUE
+  printvalcat<decltype((y))>(); //=> LVALUE
   //fum(x,y); //1  =>can be bound to (int&, int&)
   quux(x, y);
   std::cout << "***************1******************\n";
   std::cout << "***************4******************\n";
   int* ptr = &x;
-  printvalcat<decltype((ptr))>();
-  printvalcat<decltype((&x))>();
-  printvalcat<decltype((&y))>();
-  printvalcat<decltype((int{}))>();
-  printvalcat<decltype((int{}))>();
+  printvalcat<decltype((ptr))>(); //=> LVALUE
+  printvalcat<decltype((&x))>(); //=> PRVALUE
+  printvalcat<decltype((&y))>(); //=> PRVALUE
+  printvalcat<decltype((int{}))>(); //=> PRVALUE
   fum(&x, &x);
   //zoot(&x, &y);
   fum(int{}, int{});
@@ -70,14 +69,18 @@ int main () {
   noot(&x, &y);
   std::cout << "***************4******************\n";
   std::cout << "***************5******************\n";
-  printvalcat<decltype(("bir"))>();
-  printvalcat<decltype(("iki"))>();
-  printvalcat<decltype((12))>();
+  /**
+   *  All the literals, except from the string literals
+   *  are PRValues
+   */
+  printvalcat<decltype(("bir"))>(); //=> LVALUE
+  printvalcat<decltype(("iki"))>(); //=> LVALUE
+  printvalcat<decltype((12))>(); //=> PRVALUE
   //fum("bir", "iki");
   std::cout << "***************5******************\n";
   std::cout << "***************6******************\n";
-  printvalcat<decltype((std::move(x)))>();
-  printvalcat<decltype((std::move(y)))>();
+  printvalcat<decltype((std::move(x)))>(); //=> XVALUE
+  printvalcat<decltype((std::move(y)))>(); //=> XVALUE
   fum(std::move(x), std::move(y));
   //quux(std::move(x), std::move(y));
   Widget&& wrr1 = Widget{}; //=> Materialization
@@ -85,10 +88,12 @@ int main () {
   Widget w3 = std::move(w2);
   std::cout << "***************6******************\n";
 
-  printvalcat<decltype((foo()))>();
-  printvalcat<decltype((bar()))>();
-  printvalcat<decltype((baz()))>();
-  printvalcat<decltype((x+5))>();
-  printvalcat<decltype((x++))>();
-  printvalcat<decltype((--x))>();
+  std::cout << "foo() ->\t"; printvalcat<decltype((foo()))>();
+  std::cout << "bar() ->\t"; printvalcat<decltype((bar()))>();
+  std::cout << "baz() ->\t"; printvalcat<decltype((baz()))>();
+  std::cout << "x+5 ->\t"; printvalcat<decltype((x+5))>(); //=> PRVALUE
+  std::cout << "x++ ->\t"; printvalcat<decltype((x++))>(); //=> PRVALUE
+  std::cout << "--x ->\t"; printvalcat<decltype((--x))>(); //=> LVALUE
+  std::cout << "std::string{''} ->\t"; printvalcat<decltype((std::string{"literal"}))>();
+  std::cout << "'literal' ->\t"; printvalcat<decltype(("literal"))>();
 }
